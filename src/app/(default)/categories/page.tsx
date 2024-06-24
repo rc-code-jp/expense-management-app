6d02792f5200d19f27e56a0ff34e6dbe2ad45bd1
RyoCa <ryo.ca.dev@gmail.com>
2024-06-24 22:18:15 +0900

    集計処理の実装

--- src/app/(default)/categories/page.tsx
+++ src/app/(default)/categories/page.tsx
@@ -4,7 +4,6 @@ import { db } from "@/database/db";
 import { expenseCategories } from "@/database/schema";
 import { CategoryList } from "@/features/expenses/components/CategoryList";
 import { asc, desc, eq } from "drizzle-orm";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
--- src/app/(default)/report/page.tsx
+++ src/app/(default)/report/page.tsx
@@ -46,29 +46,77 @@ export default async function Page() {
 			? dateFns.format(dateFns.endOfMonth(now), dateFormat)
 			: dateFns.format(endOfWeek, dateFormat);
 
+	// トータル
 	const monthSum = items.reduce((sum, v) => sum + v.amount, 0);
-
 	const weekSum = items
 		.filter((v) => weekStartStr <= v.date && v.date <= weekEndStr)
 		.reduce((sum, v) => sum + v.amount, 0);
-
 	const todaySum = items
 		.filter((v) => todayStr === v.date)
 		.reduce((sum, v) => sum + v.amount, 0);
+
+	// 残り金額
+	const monthlyBudget = 150000; // 仮置き
+
+	// 基準日など
+	const endOfMonth = dateFns.endOfMonth(now);
+	const monthlyDaysLeft = dateFns.differenceInDays(endOfMonth, now) + 1;
+	const endDayOfWeek = dateFns.endOfWeek(now);
+	const endDayOfWeekInMonth =
+		endDayOfWeek.getMonth() !== now.getMonth()
+			? dateFns.endOfMonth(now)
+			: endDayOfWeek;
+	const startDayOfWeek = dateFns.startOfWeek(now);
+	const startDayOfWeekInMonth =
+		startDayOfWeek.getMonth() !== now.getMonth()
+			? dateFns.startOfMonth(now)
+			: startDayOfWeek;
+	const weeklyDays =
+		dateFns.differenceInDays(endDayOfWeekInMonth, startDayOfWeekInMonth) + 1;
+	const monthlyDaysLeftFromStartWeek =
+		dateFns.differenceInDays(endOfMonth, startDayOfWeekInMonth) + 1;
+
+	// 残高
+	// 今月の残高
+	const monthlyBalance = monthlyBudget - monthSum;
+	// 今月の使用率
+	const monthlyUseRate = Math.ceil((monthSum / monthlyBudget) * 100);
+	// 今週の予算（今週は含めない）
+	const weeklyBudget =
+		Math.floor((monthlyBalance + weekSum) / monthlyDaysLeftFromStartWeek) *
+		weeklyDays;
+	// 今週の残高
+	const weeklyBalance = weeklyBudget - weekSum;
+	// 今週の使用率
+	const weeklyUseRate = Math.ceil((weekSum / weeklyBudget) * 100);
+	// 今日の予算（今日は含めない）
+	const todayBudget = Math.floor((monthlyBalance + todaySum) / monthlyDaysLeft);
+	// 今日の残高
+	const todayBalance = todayBudget - todaySum;
+	// 今日の使用率
+	const todayUseRate = Math.ceil((todaySum / todayBudget) * 100);
 
 	return (
 		<div>
 			<div>
 				<p>今月の合計</p>
 				<output>{monthSum.toLocaleString()} 円</output>
+				<p>今月の残り</p>
+				<output>{monthlyBalance.toLocaleString()} 円</output>
 			</div>
+			<hr />
 			<div>
 				<p>今週の合計</p>
 				<output>{weekSum.toLocaleString()} 円</output>
+				<p>今週の残り</p>
+				<output>{weeklyBalance.toLocaleString()} 円</output>
 			</div>
+			<hr />
 			<div>
 				<p>本日の合計</p>
 				<output>{todaySum.toLocaleString()} 円</output>
+				<p>本日の残り</p>
+				<output>{todayBalance.toLocaleString()} 円</output>
 			</div>
 		</div>
 	);
