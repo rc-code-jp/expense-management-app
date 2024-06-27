714e50220159af33eb7cb9ec738b7b85896acb0d
RyoCa <ryo.ca.dev@gmail.com>
2024-06-27 21:00:15 +0900

    Loadingをkayoutで呼ぶように変更

--- src/app/(default)/calendar/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Loading() {
-	return (
-		<div className="flex size-full items-center justify-center">
-			<span className="loading loading-spinner loading-lg" />
-		</div>
-	);
-}
